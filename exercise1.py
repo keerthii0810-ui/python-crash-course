72e54cbefdd9d46a72e89b5481d4dae0d668a3f8
keerthii0810-ui <keerthii.0810@gmail.com>
2026-08-01 21:11:04 +0530

    Initial commit

---
+++ exercise1.py
@@ -0,0 +1,31 @@
+first_name="Tony"
+last_name="Stark"
+age=53
+height=5.9
+print(first_name,"is a secret superhero","and his last name","is",last_name)
+print("his age is",age)
+print("his height is",height)
+name="colonel rhodes"
+print(first_name,"is a friend of",name)
+#type conversion
+new_age=int(age)+10
+print(first_name,last_name,"will be 63 years old after 10 years")
+#another way
+print(first_name,last_name,"will be",new_age,"years old after 10 years")
+#type casting
+predicted_height=height+0.1
+print("his predicted height is",predicted_height)
+print("tony stark was able to build a suit of armor and become iron man")
+print("he built his own AI assistant named jarvis and he is a genius inventor")
+new_AI_assistant="Friday"
+print("his new AI assistant is",new_AI_assistant)
+idea="ultron"
+print("he built a new AI assistant named",idea,"but it turned out to be evil and he had to destroy it")
+appeared_in_movies=["Iron Man","Iron Man 2","The Avengers","Iron Man 3","Avengers: Age of Ultron","Captain America: Civil War","Spider-Man: Homecoming","Avengers: Infinity War","Avengers: Endgame"]
+first_appeared=appeared_in_movies[0] 
+print("he first appeared in",first_appeared)
+     
+
+
+
+
---
+++ introduction.py
@@ -0,0 +1,21 @@
+print("hi keerthana","welcome to python programming")
+#variables
+name="keerthana"
+age=19
+_class="3rd semester"
+cgpa=8.5
+print("my name is",name,"iam",age,"years old","iam studying in",_class)
+#datatypes
+print(type(name))
+print(type(age))
+print(type(_class))
+print(type(cgpa))
+isstudent=True
+print(type(isstudent))
+#input
+name=input("enter your name: ")
+print("hello",name)
+#concatenation of strings
+print("hello" + name)
+#with spaces
+print("hello "+ name)
