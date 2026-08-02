2db7f198d65d9a36cdb7398aecd4e4d7ba89db0b
keerthii0810-ui <keerthii.0810@gmail.com>
2026-08-02 21:14:41 +0530

    operator.py

--- operator.py
+++ operator.py
@@ -25,3 +25,41 @@ print(a)
 a//=2
 print(a)
 
+#operator precedence
+a=2+4*2
+print(a)
+b=(2-4)*2
+print(b)
+c=(5*5)//2+3
+print(c)
+
+#comparison operators
+a=10
+b=20
+print(a==b)
+print(a<b)
+print(a>b)
+print(a!=b)
+print(a<=b)
+print(a>=b)
+
+#logical operators
+a=2<2
+b=2>=5
+print(a or b)#either a or b is true then it will return true
+print(a and b)#both a and b should be true then it will return true
+print(not a)#if a is true then it will return false and if a is false then it will return true
+print(not b)#if b is true then it will return false and if b is false then it will return true
+
+#conditional statements
+total_marks=int(input("enter total marks:"))
+if total_marks>=90:
+    print("Grade: A")
+elif total_marks>=80:
+    print("Grade: B")
+elif total_marks>=70:
+    print("Grade: C")
+else:
+    print("Grade: D")
+print("end of program")
+
