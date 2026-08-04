914ecdf8ea152c64252856f0a64343253ba3d527
keerthii0810-ui <keerthii.0810@gmail.com>
2026-08-04 13:02:21 +0530

    range.py

---
+++ range.py
@@ -0,0 +1,3 @@
+print(range(1, 10, 2))#1 is start, 10 is stop, 2 is step. it gives the numbers starting from 1 up to (but not including) 10, incrementing by 2 each time. The output will be: [1, 3, 5, 7, 9]
+n=range(10) # This creates a range object that represents numbers from 0 to 9 (10 is not included). The output will be: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(n)
