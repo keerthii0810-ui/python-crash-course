26644a956251fcc2aadd38bf77ae5e46d3bceca3
keerthii0810-ui <keerthii.0810@gmail.com>
2026-08-04 13:19:08 +0530

    whileloop.py

--- whileloop.py
+++ whileloop.py
@@ -2,3 +2,16 @@ i=1
 while i<=4:
     print("hello","keerthana","how are you doing?")
     i+=1
+    
+    
+#structure of pyramid
+rows=1
+while rows<=5:
+    print(rows*"*")
+    rows+=1
+  
+ #structure of inverted pyramid
+rows=5
+while rows>=1:
+    print(rows*"*")
+    rows-=1      
