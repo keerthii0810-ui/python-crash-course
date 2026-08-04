6deff77da3ea0a639037f52b503ced6f8d85fed9
keerthii0810-ui <keerthii.0810@gmail.com>
2026-08-04 13:38:29 +0530

    forloop.py

---
+++ forloop.py
@@ -0,0 +1,25 @@
+n=range(1,11)
+for i in n:
+    print(i)
+
+#another way
+for i in range(11):
+    print(i)
+    
+#print even number using for loop
+for i in range(1,11):
+    if i%2==0:
+        print(i)   
+
+#another way
+for i in range(2,11,2):
+    print(i)  
+    
+#print odd number using for loop
+for i in range(1,11):
+    if i%2!=0:
+        print(i)
+#another way
+for i in range(1,11,2):
+    print(i)
+                       
