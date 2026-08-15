78334423a42106a74e73787ecb762c95ae4c895a
keerthii0810-ui <keerthii.0810@gmail.com>
2026-08-15 21:10:04 +0530

    set.py

--- set.py
+++ set.py
@@ -1,7 +1,6 @@
 my_set = {1, 2, 3, 4, 5, 5, 6, 6}
 print(my_set) #printing the set. Output will be {1, 2,
 print(my_set,type(my_set)) #printing the set and its type
-print(my_set.count(5)) #this will raise an error because sets do not have a count method
 print(my_set.add(60)) #this will add the element 60 to the set
 print(len(my_set)) #finding the length of the set
 
