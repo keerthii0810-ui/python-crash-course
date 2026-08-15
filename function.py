20120bd3547810e346e6ead65891a2ccfac78bbf
keerthii0810-ui <keerthii.0810@gmail.com>
2026-08-15 22:36:19 +0530

    function.py

---
+++ function.py
@@ -0,0 +1,13 @@
+def last_name(name):
+    full_name = name + " M"
+    return(full_name)
+
+print(last_name("Keerthana"))
+
+
+#example2
+def updated_roll_no(previous_roll_no):
+    current_roll_no=previous_roll_no+[69,70,71]
+    return(current_roll_no)
+
+print(updated_roll_no(59,60,61))
