49a78cd5393b6097bef58e810717648f257f1f6b
keerthii0810-ui <keerthii.0810@gmail.com>
2026-08-15 21:07:20 +0530

    set.py

--- set.py
+++ set.py
@@ -1,8 +1,7 @@
-#creating a set
-my_set = {1, 2, 3, 4, 5,5,6,6}#creating a set with duplicate values. The duplicates will be removed automatically.
+my_set = {1, 2, 3, 4, 5, 5, 6, 6}
 print(my_set) #printing the set. Output will be {1, 2,
 print(my_set,type(my_set)) #printing the set and its type
 print(my_set.count(5)) #this will raise an error because sets do not have a count method
-print(my_set.append(60)) #this will add the element 60 to the set
+print(my_set.add(60)) #this will add the element 60 to the set
 print(len(my_set)) #finding the length of the set
 
