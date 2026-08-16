d3f3ac074967d7538c206cf88dfdda27a603b3f2
keerthii0810-ui <keerthii.0810@gmail.com>
2026-08-16 21:45:00 +0530

    inbuilt.py

---
+++ inbuilt.py
@@ -0,0 +1,5 @@
+#inbuilt function means  the arguments that are already present in python
+print("hi","keerthana")#here print is an inbuilt function 
+print(len([1,2,3]))#here len() is an inbuilt funtion
+print(max(1,2,3))#max() is an inbuilt function
+print(min(1,2,3))#min() is an inbuilt function
