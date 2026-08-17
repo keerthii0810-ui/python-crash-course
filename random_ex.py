9268f1c1bf85b4513ca1866f8c3b18866418c9b0
keerthii0810-ui <keerthii.0810@gmail.com>
2026-08-17 19:23:02 +0530

    random_ex.py

--- random_ex.py
+++ random_ex.py
@@ -1,3 +1,20 @@
 import random
 
-print(random.randint(1, 10))
+print(random.random())#0 to 1
+
+import random
+print(random.randint(1,5))
+
+import random
+name=input("enter your name:")
+paasword=(input("enter your password:"))
+customer_requirement=int(input("enter your requirement:"))
+stocks_available=random.randint(1,200)
+price=random.randint(1,20000)
+print("currently available",stocks_available)
+if customer_requirement>=stocks_available:
+        print("stock is not available")
+else:
+        print("stock is available")
+        print("hello",name,"your requirement of products are available")
+print("The amount to be paid for the requirement:",price)        
