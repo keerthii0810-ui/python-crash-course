add4338cc11f2629da752d4919c52c7166dbf9fc
keerthii0810-ui <keerthii.0810@gmail.com>
2026-08-17 21:30:14 +0530

    random_ex.py

--- random_ex.py
+++ random_ex.py
@@ -8,6 +8,13 @@ print(random.randint(1,5))
 import random
 name=input("enter your name:")
 password=input("enter your password:")
+skincare_products=["dot&key","kyliecosmetics","rhode","rarebeauty","cetaphil"]
+my_choice=input("enter your required product:")
+if my_choice not in skincare_products:
+    print("the product is not available")
+    exit()
+else:
+    print("the product is available")
 customer_requirement=int(input("enter your requirement:"))
 stocks_available=random.randint(1,200)
 price=random.randint(1,20000)
@@ -15,6 +22,6 @@ print("currently available",stocks_available)
 if customer_requirement>=stocks_available:
         print("stock is not available")
 else:
-        print("stock is available")
+        print("congratulations!.the stock is available")
         print("hello",name,"your requirement of products are available")
         print("The amount to be paid for the requirement:",price)        
