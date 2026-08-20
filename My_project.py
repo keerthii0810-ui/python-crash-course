254b711c7a6867e9280dceb6cc22c6c0490e3ad1
keerthii0810-ui <keerthii.0810@gmail.com>
2026-08-20 13:55:09 +0530

    My_project.py

--- My_project.py
+++ My_project.py
@@ -14,19 +14,29 @@ my_choice1=input("Enter your required brand: ")
 my_choice2=input("Enter your required product: ")
 if my_choice2 in skincare_products:
     index=skincare_products.index(my_choice2)
-    print("skincare_product:",skincare_products[index])
-if my_choice1 not in skincare_brand:
-    print("the product is not available")
+    brand_index=skincare_brand.index(my_choice1)
+    if index==brand_index:
+     print("skincare_product:",skincare_products[index])
+     print("the product is availabe")
+    else:
+     print("the product is not available")
+     exit()
+else:
+    print("invalid choice")
     exit()
+if my_choice1  in skincare_brand:
+    index=skincare_brand.index(my_choice1)
+    print("the brand is available")
 else:  
-    print("the product is available")
+    print("the brand is not available")
+    exit()
 customer_requirement=int(input("Enter your requirement: "))
 index=skincare_brand.index(my_choice1)
 price=price_products[index]*customer_requirement
 stocks_available=random.randint(1,200)
 print("Currently available: ",stocks_available)
 if customer_requirement>=stocks_available:
-        print("stock is not available")
+        print("stock is not  available")
 else:
         print("Congratulations!...the stock is available")
         print("Hello",name,"your requirement of products are available.")
