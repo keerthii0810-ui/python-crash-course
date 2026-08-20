bdc361c7eca77cee9bac1a3ec6b34cb84cfcce3c
keerthii0810-ui <keerthii.0810@gmail.com>
2026-08-20 11:43:39 +0530

    random_ex.py

--- random_ex.py
+++ random_ex.py
@@ -8,16 +8,20 @@ print(random.randint(1,5))
 import random
 name=input("enter your name:")
 password=input("enter your password:")
-skincare_products=["dot&key","kyliecosmetics","rhode","rarebeauty","cetaphil"]
-my_choice=input("enter your required product:")
-if my_choice not in skincare_products:
+skincare_brand=["dot&key","kyliecosmetics","rhode","rarebeauty","cetaphil"]
+skincare_products=["sunscreen","perfume","lipbalm","lipstick","facewash"]
+price_products=[700,2200,600,1200,650]
+my_choice=input("enter your required brand:")
+if my_choice not in skincare_brand:
     print("the product is not available")
     exit()
 else:
+    index=skincare_brand.index(my_choice)
+    print("skincare_product:",skincare_products[index])
+    price=price_products[index]   
     print("the product is available")
 customer_requirement=int(input("enter your requirement:"))
 stocks_available=random.randint(1,200)
-price=random.randint(1,20000)
 print("currently available",stocks_available)
 if customer_requirement>=stocks_available:
         print("stock is not available")
