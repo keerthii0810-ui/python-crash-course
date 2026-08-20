3781401b2d22059971b8b6ca73430a7ea88b36b8
keerthii0810-ui <keerthii.0810@gmail.com>
2026-08-20 12:25:32 +0530

    random_ex.py

--- random_ex.py
+++ random_ex.py
@@ -6,26 +6,35 @@ import random
 print(random.randint(1,5))
 
 import random
-name=input("enter your name:")
-password=input("enter your password:")
+name=input("Enter your name: ")
+password=input("Enter your password: ")
 skincare_brand=["dot&key","kyliecosmetics","rhode","rarebeauty","cetaphil"]
+print("Available brands: ")
+for brand in skincare_brand:
+ print(brand)
 skincare_products=["sunscreen","perfume","lipbalm","lipstick","facewash"]
+print("Available products: ")
+for product in skincare_products:
+ print(product)
 price_products=[700,2200,600,1200,650]
-my_choice=input("enter your required brand:")
-if my_choice not in skincare_brand:
+my_choice1=input("Enter your required brand: ")
+my_choice2=input("Enter your required product: ")
+if my_choice2 in skincare_products:
+    index=skincare_products.index(my_choice2)
+    print("skincare_product:",skincare_products[index])
+if my_choice1 not in skincare_brand:
     print("the product is not available")
     exit()
-else:
-    index=skincare_brand.index(my_choice)
-    print("skincare_product:",skincare_products[index])
-    price=price_products[index]   
+else:  
     print("the product is available")
-customer_requirement=int(input("enter your requirement:"))
+customer_requirement=int(input("Enter your requirement: "))
+index=skincare_brand.index(my_choice1)
+price=price_products[index]*customer_requirement
 stocks_available=random.randint(1,200)
-print("currently available",stocks_available)
+print("Currently available: ",stocks_available)
 if customer_requirement>=stocks_available:
         print("stock is not available")
 else:
-        print("congratulations!.the stock is available")
-        print("hello",name,"your requirement of products are available")
+        print("Congratulations!...the stock is available")
+        print("Hello",name,"your requirement of products are available.")
         print("The amount to be paid for the requirement:",price)        
